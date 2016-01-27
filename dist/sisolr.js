e297f8f41d4d07baf2a2da34b10ec18d82d67fb5
cincun-laptop <yasinmkarya@gmail.com>
2016-01-27 12:58:23 +0700

    penambahan panel di analisys

--- dist/sisolr.js
+++ dist/sisolr.js
@@ -1,5 +1,5 @@
 var app = angular.module('Sisolr', ['ui.bootstrap','ui.bootstrap.datetimepicker', 'ngMessages']);
-var server = 'http://10.14.211.5:8983',
+var server = 'http://127.0.0.1:8080',
     path = '/solr/core0';
 
 function formatTgl(str) {
@@ -127,6 +127,8 @@ app.controller('search', function($scope, $http) {
           }
           _getJSONP(0, _view);
         }
+
+        $scope.currentPage = 1;    
     }
 
     $scope.now = function() {
